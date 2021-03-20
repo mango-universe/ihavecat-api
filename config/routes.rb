0a8e731e3210fa0970b65c97c6d8b0ab3de11cb6
Ryan <hyogol@hotmail.com>
2021-03-21 03:43:58 +0900

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+
+  sidekiq_user = Rails.application.credentials.dig(Rails.env.to_sym, :sidekiq, :user)
+  sidekiq_password = Rails.application.credentials.dig(Rails.env.to_sym, :sidekiq, :password)
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(sidekiq_user)) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(sidekiq_password))
+  end
+
+  # Sidekiq::Web.set :session_secret, Rails.application.credentials[:secret_key_base]
+  mount Sidekiq::Web, at: "/sidekiq"
+
+  get '/health', to: proc { [200, {}, ['']] }
+
+
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
