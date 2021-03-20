0db01f83c124589aa25abb9be279f1c648bda21e
Ryan <hyogol@hotmail.com>
2021-03-21 03:18:00 +0900

    modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
