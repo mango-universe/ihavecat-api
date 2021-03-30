5d5f23a1f6d55fcc0379fcd53f3cd0ebc828ece0
Ryan <hyogol@hotmail.com>
2021-03-30 15:54:31 +0900

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
 
-  sidekiq_user = Rails.application.credentials.dig(Rails.env.to_sym, :sidekiq, :user)
-  sidekiq_password = Rails.application.credentials.dig(Rails.env.to_sym, :sidekiq, :password)
+  sidekiq_user = Rails.application.credentials.dig(:sidekiq, :user)
+  sidekiq_password = Rails.application.credentials.dig(:sidekiq, :password)
 
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(sidekiq_user)) &
